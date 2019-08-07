25ab63b7e78860531e9b4553ac72eaeea6a13418
Ravi Teja Kumar Isetty <iravitejakumar@gmail.com>
2019-08-07 23:58:33 +0530

    Added emotion.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { Global, css } from '@emotion/core';
+
 import Header from "./header"
 import "./layout.css"
 
